a7571e61954e68f4fcd3612f810ff739dbc227ef
Dominic Cobb <dominicshajaa@icloud.com>
2020-09-23 11:30:28 -0700

    mobile fixed. made nav sticky

--- pages/about.js
+++ pages/about.js
@@ -88,6 +88,7 @@ export default function About(props) {
                 <li>CSS</li>
                 <li>Sass</li>
                 <li>Kotlin</li>
+                <li>Swift</li>
               </ul>
             </div>
             <div className={styles.aboutSkill}>
